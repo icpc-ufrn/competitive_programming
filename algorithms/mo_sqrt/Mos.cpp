ef358b9fa974400c1f1d9dadafc8be67be838d87
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-05-10 11:24:57 -0300

    rmvd code

--- algorithms/mo_sqrt/Mos.cpp
+++
@@ -1,7 +0,0 @@
-bool mosort(pair<pair<int,int>,int> A, pair<pair<int,int>,int> B){
-    if (A.first.first / S != B.first.first / S)
-        return (A.first.first / S) < (B.first.first / S);
-    return A.first.second > B.first.second;
-}
-
-sort(qs, qs+Q, mosort);
--- algorithms/polynomial/README.md
+++
@@ -1,5 +0,0 @@
-# Polynomials
-
-### Roots of a quadratic function
-
-Avoid `-b+sqrt(delta)` if `-b` and `sqrt(delta)` have opposite signals due to catastrophic cancellation. 
